aeef70e601004926038d537c91cf37c6813c38de
vikram67 <41691338+vikram67@users.noreply.github.com>
2023-08-18 19:53:16 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -5,5 +5,5 @@
     // Your function logic here
     echo "Project Name: ${ProjectName}"
     echo "Project Version: ${ProjectVersion}"
-    echo "Jenkins Account: ${JenkinsAccount.account}"
+    echo "Jenkins Account: ${BUILDTAG"
 }
