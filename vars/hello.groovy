b9c93ff2ff936bfde45804b31632ccdd16f7360a
vikram67 <41691338+vikram67@users.noreply.github.com>
2023-08-18 19:51:10 +0530

    Update hello.groovy

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,8 +1,9 @@
-def call(Map params) {
+ def call(Map params) {
     def projectName = params.ProjectName
     def projectVersion = params.ProjectVersion
 
     // Your function logic here
     echo "Project Name: ${ProjectName}"
     echo "Project Version: ${ProjectVersion}"
+    echo "Jenkins Account: ${JenkinsAccount.account}"
 }
